47eda7cc5f88420305c680762003438df3d38f11
berrrchik <berrrchik@mail.ru>
2025-11-12 20:32:51 +0300

    Initial Commit

---
+++ RussianSignLanguageDictionary/RussianSignLanguageDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RussianSignLanguageDictionaryApp.swift
+//  RussianSignLanguageDictionary
+//
+//  Created by Анастасия Берчик on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RussianSignLanguageDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
