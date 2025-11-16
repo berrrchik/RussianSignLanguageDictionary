4d1f4c7c75e48a78ec37eb8a35b3f15e7abc5e38
berrrchik <berrrchik@mail.ru>
2025-11-16 23:29:59 +0300

    refactor: reorganize project structure

--- RussianSignLanguageDictionary/RussianSignLanguageDictionaryApp.swift
+++ RussianSignLanguageDictionary/RussianSignLanguageDictionaryApp.swift
@@ -1,17 +1,23 @@
-//
-//  RussianSignLanguageDictionaryApp.swift
-//  RussianSignLanguageDictionary
-//
-//  Created by Анастасия Берчик on 11/12/25.
-//
-
 import SwiftUI
 
 @main
 struct RussianSignLanguageDictionaryApp: App {
+    init() {
+        setupDependencies()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    // MARK: - Dependency Injection Setup
+    private func setupDependencies() {
+        let container = DependencyContainer.shared
+        
+        #if DEBUG
+        print("Dependencies container initialized")
+        #endif
     }
 }
