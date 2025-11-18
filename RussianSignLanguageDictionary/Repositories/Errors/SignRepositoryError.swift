ddac2a00dbb3a19c79eea60b061d7919fc304bfd
berrrchik <berrrchik@mail.ru>
2025-11-18 18:45:39 +0300

    refactor: simplify error handling

--- RussianSignLanguageDictionary/Repositories/Errors/SignRepositoryError.swift
+++ RussianSignLanguageDictionary/Repositories/Errors/SignRepositoryError.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 /// Ошибки репозитория жестов
-enum SignRepositoryError: LocalizedError {
+/// Маппинг в ErrorMessageMapper
+enum SignRepositoryError: Error {
     /// JSON файл не найден в Bundle
     case fileNotFound
     
@@ -13,18 +14,5 @@ enum SignRepositoryError: LocalizedError {
     
     /// Неверный формат данных
     case invalidDataFormat
-    
-    var errorDescription: String? {
-        switch self {
-        case .fileNotFound:
-            return "JSON файл с данными о жестах не найден в приложении"
-        case .unableToReadFile:
-            return "Не удалось прочитать файл с данными о жестах"
-        case .decodingError(let error):
-            return "Ошибка при декодирования данных: \(error.localizedDescription)"
-        case .invalidDataFormat:
-            return "Неверный формат данных в JSON файле"
-        }
-    }
 }
 
