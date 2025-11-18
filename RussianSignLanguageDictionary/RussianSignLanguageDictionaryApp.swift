2859ddcf5b2e7b6170af789542d0d1d4f8712325
berrrchik <berrrchik@mail.ru>
2025-11-18 18:41:24 +0300

    refactor: simplify app entry point

--- RussianSignLanguageDictionary/RussianSignLanguageDictionaryApp.swift
+++ RussianSignLanguageDictionary/RussianSignLanguageDictionaryApp.swift
@@ -2,22 +2,9 @@ import SwiftUI
 
 @main
 struct RussianSignLanguageDictionaryApp: App {
-    init() {
-        setupDependencies()
-    }
-    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
-    }
-    
-    // MARK: - Dependency Injection Setup
-    private func setupDependencies() {
-        let container = DependencyContainer.shared
-        
-        #if DEBUG
-        print("Dependencies container initialized")
-        #endif
     }
 }
