68e7c4aa55b10131d280368ef0e1220b42ac5799
berrrchik <berrrchik@mail.ru>
2026-04-27 19:07:24 +0300

    fix: increase video session timeouts

--- RussianSignLanguageDictionary/Services/Network/VideoSessionFactory.swift
+++ RussianSignLanguageDictionary/Services/Network/VideoSessionFactory.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 enum VideoSessionFactory {
-    static let requestTimeout: TimeInterval = 5
-    static let resourceTimeout: TimeInterval = 12
+    static let requestTimeout: TimeInterval = 10
+    static let resourceTimeout: TimeInterval = 20
 
     static func makeSession() -> URLSession {
         let configuration = URLSessionConfiguration.default
